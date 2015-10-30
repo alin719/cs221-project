a4128880bbd474ce3ab869ab2cfa44b225b0c7c2
Austin <aray@cs.stanford.edu>
2015-10-29 23:06:33 -0700

    Hello world! ('sandbox.py' added)
    Added the file sandbox.py, which we can use as a kind of notebook to
    document our work trying to learn the various packages and develop the
    commands we will need.

---
+++ sandbox.py
@@ -0,0 +1,13 @@
+# Authors: Austin Ray, Bruno De Martino, Alex Lin
+# File: sandbox.py
+# ----------------
+# This file exists so we can play around with different packages.
+# As we develop useful functions, we will port groups of similar functions
+# from sandbox.py into other package files.
+#
+# Note: Commenting encouraged so other people can learn from what you've done!
+
+import collections
+import numpy, scipy, math
+
+print "Hello world!""
