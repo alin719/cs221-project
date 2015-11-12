ba04b9f36bcef9d6f41844ce10cb8c17a3118835
Austin <aray@cs.stanford.edu>
2015-11-12 04:39:15 -0800

    Revert "Made improvements to now also connect with the government API. Still struggling to get ALL data from both APIs with one call, might need to break it down into several small calls instead."
    This reverts commit 49d1c4cb9b9ba9c1f7dc21f77c8ed9a6f553d00c.

--- database.py
+++ database.py
@@ -16,25 +16,13 @@ import math, random, collections, requests, json
 def createRecipeDatabase(filename, maxResults):
 	apiString = "http://api.yummly.com/v1/api/recipes?_app_id=4d1d7424&_app_key=419a5ef2649eb3b6e359b7a9de93e905&q=&maxResult=%d" % maxResults
 	request = requests.get(apiString)
-	assert request.status_code == 200
 	print request.headers
+	assert request.status_code == 200
 	allRecipesFile = open(filename, 'w+')
 	allRecipesFile.write(request.content)
 	allRecipesFile.close()
 
-# Function: Create Nutritional Database
-# -----------------------
-# 
-def createNutritionalDatabase(filename, maxResults):
-	assert maxResults <= 1500
-	apiString = "http://api.nal.usda.gov/ndb/list?format=json&lt=f&max=%d&sort=n&api_key=5YbfzajkZSaGWi7hibcD4Nq1EXSGHRtZP5Pvlkvv" % maxResults
-	request = requests.get(apiString)
-	assert request.status_code == 200
-	allNutritionalFile = open(filename, 'w+')
-	allNutritionalFile.write(request.content)
-	allNutritionalFile.close()
-
-# Function: Test Recipe Database
+# Function: Create Recipe Database
 # -----------------------
 # Calls createRecipeDatabase with filename 'allRecipes.json' and maxResults = 100,
 # then proceeds to print all ingredients from each recipe fetched
@@ -42,7 +30,7 @@ def createNutritionalDatabase(filename, maxResults):
 # maxResults: max number of recipes from Yummly database
 # filename: name of the file the recipes will be written into
 # See example at: https://developer.yummly.com/documentation/search-recipes-response-sample
-def testRecipeDatabase():
+def testDatabase():
 	filename = 'allRecipes.json'
 	createRecipeDatabase(filename, 100)
 	readFile = open(filename)
@@ -57,27 +45,3 @@ def testRecipeDatabase():
 			print ingredient
 		print "********** %d *********" % count
 		count += 1
-
-# Function: Test Nutritional Database
-# -----------------------
-# 
-def testNutritionalDatabase():
-	filename = 'allNutritional.json'
-	createNutritionalDatabase(filename, 100)
-	readFile = open(filename)
-	allNutritional = json.loads(readFile.read())
-	foodList = allNutritional["list"]
-	items = foodList["item"]
-	for itemObject in items:
-		itemName = itemObject["name"]
-		itemId = itemObject["id"] 
-		print itemName + ": " + itemId
-
-
-# TODO: since neither API let's me call the WHOLE database, I need to figure out a way to do
-# several small calls, and be able to merge them in my allRecipes.json and allNutritional.json
-# finals, so they only have one master object each.
-
-
-
-
